e2e4eda779d2946aeb4764744dcad5e9cb49eaa4
舟叙 <stevenenenen@163.com>
2026-04-11 23:30:22 +0800

    bug修复

--- TesseraScript/dependency-loader.js
+++ TesseraScript/dependency-loader.js
@@ -5,9 +5,7 @@ await dv.view("TesseraScript/core/css");
 await dv.view("TesseraScript/components/card/index");
 await dv.view("TesseraScript/components/progress/index");
 await dv.view("TesseraScript/components/heatmap/index");
-await dv.view("TesseraScript/components/heatmap-v2/index");
 
 card = Tessera.require("components/card");
 progress = Tessera.require("components/progress");
 heatmap = Tessera.require("components/heatmap");
-heatmapV2 = Tessera.require("components/heatmap-v2");
